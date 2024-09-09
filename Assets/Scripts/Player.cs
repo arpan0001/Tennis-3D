b10bc0a69c75e4977c912d5308bcba791759d744
arpan0001 <arpan21csu561@ncuindia.edu>
2024-09-09 17:42:21 +0530

    Ball

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,32 +5,66 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    Vector2 moveVector;
-    float speed = 3f;
-    public float moveSpeed = 3f;
-    // Start is called before the first frame update
-    public void InputPlayer(InputAction.CallbackContext _context){
-        moveVector = _context.ReadValue<Vector2>();
-    }
-    void Start()
-    {
-        
-    }
+    Vector2 moveVector;        // Stores joystick movement input
+    public Transform aimTarget;
+    float speed = 3f;          // Movement speed
+    float force = 10f;         // Force applied to the ball
+    bool hitting;              // Indicates if the player is hitting
 
-    // Update is called once per frame
-    private  void Update()
+    private void Update()
     {
+        // Get keyboard input for movement
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-       if (h != 0 || v != 0)
+        // Check if the player is hitting
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            transform.Translate(new Vector3(h, 0, v) * speed * Time.deltaTime); 
+            hitting = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.F))
+        {
+            hitting = false;
         }
 
-        Vector3 movement = new Vector3(moveVector.x, 0, moveVector.y);
-        movement.Normalize();
-        transform.Translate(moveSpeed * movement * Time.deltaTime);
+        // Combine joystick and keyboard input
+        Vector3 movementInput = new Vector3(moveVector.x + h, 0, moveVector.y + v);
+
+        // Handle aimTarget movement when hitting
+        if (hitting)
+        {
+            Vector3 aimMovement = new Vector3(movementInput.x, 0, 0) * speed * 2 * Time.deltaTime;
+            aimTarget.Translate(aimMovement);  // Move aim target horizontally
+        }
+        // Handle player movement when not hitting
+        else if (movementInput.x != 0 || movementInput.z != 0)
+        {
+            Vector3 movement = new Vector3(movementInput.x, 0, movementInput.z) * speed * Time.deltaTime;
+            transform.Translate(movement);  // Move player using combined input
+        }
+    }
+
+    // Handle collision with the ball
+   private void OnTriggerEnter(Collider other)
+ {
+    if (other.CompareTag("Ball"))
+    {
+        Rigidbody ballRigidbody = other.GetComponent<Rigidbody>();
         
+        if (ballRigidbody != null)
+        {
+            // Calculate direction from the player to the aimTarget
+            Vector3 dir = aimTarget.position - transform.position;
+            // Apply force to the ball in the direction of the aimTarget, with an upward boost
+            ballRigidbody.velocity = dir.normalized * force + new Vector3(0, 10, 0);
+        }
+    }
+ }
+
+
+    // New input system callback for joystick movement
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveVector = context.ReadValue<Vector2>();
     }
 }
